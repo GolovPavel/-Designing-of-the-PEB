879e9d51f71df3cfa518ce5d617f6bd755f1ddb3
golovpavel <golovpasha@gmail.com>
2017-11-08 15:51:18 +0300

    'temperature_distribution_inner_out'

--- Heat calculation/distributions.py
+++ Heat calculation/distributions.py
@@ -4,11 +4,12 @@ import utils
 import sympy as sp
 from iapws import IAPWS97
 
-step = 0.05
+step = 0.01
 z = np.arange(0, 1.3 + step, step)
 q = np.zeros(z.size)
 t_water = np.zeros(z.size)
 t_sh_outer = np.zeros(z.size)
+t_sh_inner = np.zeros(z.size)
 
 def q_distribution_over_the_height_calc(data):
     global q
@@ -25,12 +26,12 @@ def q_distribution_over_the_height_calc(data):
 def temp_distribution_of_water_calc(data):
     global t_water
 
-    qMax = data["kV"] * data["qSr"] * 1e6
+    qMax = data["kV"] * data["qSr"] *1e6
 
     f0 = -1 / np.pi * (data["Haz"] + 2 * data["deltaZ"]) * np.sin(np.pi / 2 * ((data["Haz"]) / (data["Haz"] + 2 * data["deltaZ"])))
     f = -1 / np.pi * (data["Haz"] + 2 * data["deltaZ"]) * np.sin(np.pi / 2 * ((data["Haz"] - 2 * z) / (data["Haz"] + 2 * data["deltaZ"]))) - f0
 
-    t_water = data["tVhR"] + (data["kG"] * data["Pt"] * qMax) / (data["kQ"] * data["kR"] * data["Gtn"] * data["Cp"]) * f
+    t_water = data["tVhR"] + (data["kG"] * data["Pt"] * qMax) / (data["kQ"] * data["kR"] * Gtvsm * data["Cp"]) * f
 
     plt.plot(z, t_water)
     plt.grid(True)
@@ -42,27 +43,60 @@ def temp_distribution_of_water_calc(data):
 def temp_distribution_of_outer_shell(data):
     global t_sh_outer
 
+    #TODO найти ошибку
     for i in range(z.size):
         water = IAPWS97(T = t_water[i] + 273.0, P = data["P1c"])
         nu = water.Liquid.nu
         mu = water.Liquid.mu
-        alfa = water.Liquid.alfa
+        Pr = water.Liquid.Prandt
         lambda_water = water.Liquid.k
 
-        Re = data["w"] * data["d"] / nu
-        Pr = nu / alfa
-        Nu = (0.0165 + 0.02 * (1 - 0.91 / data["x"] ** 2) * data["x"] ** 0.15) * Re ** 0.8 * Pr ** 0.4
+        Re = Wtvsm * data["d"] / nu
+        # Nu = (0.0165 + 0.02 * (1 - 0.91 / data["x"] ** 2) * data["x"] ** 0.15) * Re ** 0.8 * Pr ** 0.4
+        Nu = 0.023 * Re ** 0.8 * Pr ** 0.4
         a = Nu * lambda_water / data["d"]
         Ra = 1 / a
         t_sh_outer[i] = t_water[i] + q[i] * Ra
 
-        #TODO поверхностное кипение
+        # if (t_sh_outer[i] > ts[0]):
+        #     aBoil = 4.32 * (data["P1c"] ** 0.14 + 1.28 * 10 ** -2 * data["P1c"] ** 2) * q[i] ** 0.7
+        #     RaBoil = 1 / aBoil
+        #     t_sh_outer[i] = ts[0] + q[i] * RaBoil
 
     plt.plot(z, t_sh_outer)
+    plt.plot(z, ts)
+    plt.grid(True)
+    plt.xlabel(r'z, $м$', fontsize=14, fontweight='bold')
+    plt.ylabel(r't, $^oC$', fontsize=14, fontweight='bold')
     plt.show()
+
+def temp_distribution_of_inner_shell(data):
+    global t_sh_inner
+    RShell = data["deltaShell"] / data["lambdaShell"]
+    print(RShell)
+    t_sh_inner = t_sh_outer + q * RShell
+
+    plt.plot(z, t_sh_inner)
+    plt.grid(True)
+    plt.xlabel(r'z, $м$', fontsize=14, fontweight='bold')
+    plt.ylabel(r't, $^oC$', fontsize=14, fontweight='bold')
+    plt.show()
+
 
 data = {}
 utils.fill_dict_from_file(data, "distributionsInput.txt")
+
+#Constants calculation
+Gtvsm = data["Gtn"] * data["kR"]
+Wtvsm = data["w"] * data["kR"]
+
 q_distribution_over_the_height_calc(data)
 temp_distribution_of_water_calc(data)
+
+water=IAPWS97(P=data["P1c"], x = 0)
+ts = list(map(lambda x: water.T - 273, np.zeros(z.size)))
+
 temp_distribution_of_outer_shell(data)
+temp_distribution_of_inner_shell(data)
+
+print(max(t_sh_outer))
--- Heat calculation/test.py
+++ Heat calculation/test.py
@@ -1,4 +1,4 @@
 from iapws import IAPWS97
 
-water=IAPWS97(T=300,P=10)
-print(water.Liquid.mu)
+water=IAPWS97(T = 320 + 273.0, P = 12.0)
+print(water.Liquid.nu)
