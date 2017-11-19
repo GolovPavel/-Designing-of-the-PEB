c69fd90ae2743461e5951fe6da0819759382a3ed
golovpavel <golovpasha@gmail.com>
2017-11-19 21:55:11 +0300

    'heat_crysis'

--- Heat calculation/Heat crysis/test.py
+++ Heat calculation/Heat crysis/test.py
@@ -2,14 +2,8 @@ from iapws import IAPWS97
 
 water1 = IAPWS97(T = 294.7 + 273, P = 12.7)
 water2 = IAPWS97(P = 12.7, x = 0.0)
+res = (water1.h - water2.h) / 1.1504e+006 * 1e3
 
-i = water1.h
-print(i)
-ish = water2.h
-print(ish)
-r = 1.1504e+006
-
-res = (i - ish) / r * 1e3
 print(res)
 
 print((1.3111e+006 - 1.5195e+006) / 1.1504e+006)
